2006866635b35428099410185f611ab19c7b1ec4
TomasCO21 <tomascardonaortiz@gmail.com>
2023-10-18 20:25:47 +0000

    ejemplo

--- src/componentes/cursoanti.jsx
+++ src/componentes/cursoanti.jsx
@@ -9,7 +9,7 @@ function Cursoanti() {
           <h5 className="card-title">Card title</h5>
           <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
           <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-        </div>
+        </div> 
       </div>
       <div className="card">
         <div className="card-body">
